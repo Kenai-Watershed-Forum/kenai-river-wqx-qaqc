a7c826a8b4628db5ec5b4a7aa11ef1371f2a920a
benjamin-meyer-ak <alaskasaurusrex.ak@gmail.com>
2024-02-14 12:11:40 -0900

    commit

---
+++ other/misc/formatting_notes_template.R
@@ -0,0 +1,53 @@
+# Project ID (static)***
+
+# Monitoring Location ID (static, already present)
+
+# Activity Media Name	(static)***
+
+# Activity Media Subdivision Name	(static)***
+
+# Activity ID	(concatenated) (unique identifier) (limit of X  characters)
+
+# Activity Start Date	(static, specific to SGS)
+
+# Activity Start Time	(static, specific to SGS)
+
+# Activity Start Time Zone***
+
+# Activity Depth/Height Measure	***
+
+# Activity Depth/Height Unit	***
+
+# Activity Comment ***
+
+# Activity Horizontal Collection Method	***
+
+# Activity Horizontal Reference Datum	***
+
+# Characteristic Name	
+
+# Result Analytical Method ID,	and
+# Result Analytical Method Context
+
+# Result Value	
+
+# Result Unit	
+
+# Result Qualifier
+
+# Result Weight Basis	***
+
+# Result Sample Fraction	
+
+# Method Speciation	
+
+# Result Value Type	***
+
+# Sample Collection Equipment Name	
+
+
+################### Part B.2: Create Consistent Sample Location Names for SWWTP FC data #####################
+
+############### Part B.3: Assign miscellaneous calculated columns for 2023 SWWTP FC (needed for next steps) ###############
+
+################### Part B.4: Proceed left to right across column names to calculate as needed #####################
